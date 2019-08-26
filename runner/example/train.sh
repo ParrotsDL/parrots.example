f0c58c6a2af93cb8674cd7535374fce0f2caade0
wuwenli <wuwenli@sensetime.com>
2019-08-26 15:08:20 +0800

    refactor

--- runner/example/train.sh
+++ runner/example/train.sh
@@ -1,4 +1,4 @@
-mkdir -p log
+mkdir -p log/example
 now=$(date +"%Y%m%d_%H%M%S")
 
 ROOT=.
@@ -7,10 +7,10 @@ export PYTHONPATH=$pyroot:$PYTHONPATH
 
 name=$3
 g=$(($2<8?$2:8))
-cfg=$ROOT/configs/daily/example/${name}.yaml
+cfg=$ROOT/configs/example/${name}.yaml
 
 OMPI_MCA_mpi_warn_on_fork=0 GLOG_vmodule=MemcachedClient=-1 \
 srun --mpi=pmi2 -p $1 --job-name=${name} \
-    --gres=gpu:$g -n$2 --ntasks-per-node=$g \
+    --gres=gpu:$g -n$2 --ntasks-per-node=$g --job-name=$4 \
     python -u $ROOT/models/parrots.example/tools/main.py --config ${cfg} \
-    2>&1 | tee log/train_${name}.log-$now
+    2>&1 | tee log/example/train_$4.log-$now
