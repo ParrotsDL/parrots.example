f350179f51bec63aca4c7a367e2aa9c76f2c6f66
zhuyuanhao <zhuyuanhao@sensetime.com>
2019-10-08 19:04:41 +0800

    Update train.sh

--- runner/pod/train.sh
+++ runner/pod/train.sh
@@ -2,14 +2,15 @@
 mkdir -p log/pod/
 
 T=`date +%m%d%H%M`
-ROOT=~/parrots.test
-cfg=$ROOT/configs/pod/rfcn-R101-ohem-deform-1x.yaml
+ROOT=.
+name=$3
+cfg=$ROOT/configs/pod/${name}.yaml
 
 pyroot=$ROOT/models/pytorch-object-detection
 export PYTHONPATH=$pyroot:$PYTHONPATH
 
-srun --mpi=pmi2 -p $1 -n16 --gres=gpu:8 --ntasks-per-node=8 \
-    --job-name=R50-C4 \
+OMPI_MCA_mpi_warn_on_fork=0 GLOG_vmodule=MemcachedClient=-1 \
+srun --mpi=pmi2 -p $1 -n$2 --gres=gpu:8 --ntasks-per-node=8 --job-name=${name} \
 python $ROOT/models/pytorch-object-detection/tools/train_val.py \
   --config=$cfg \
-  2>&1 | tee log/pod/train.rfcn-R101-ohem-deform-1x.log_latest.$T
+  2>&1 | tee log/pod/train.{name}.log.$T
