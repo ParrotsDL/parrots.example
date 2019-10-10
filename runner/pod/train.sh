ee67bff0e328ff89bdc9ee51d5558b50d05fe49b
zhuyuanhao <zhuyuanhao@sensetime.com>
2019-10-10 13:05:30 +0800

    Update train.sh

--- runner/pod/train.sh
+++ runner/pod/train.sh
@@ -13,4 +13,4 @@ OMPI_MCA_mpi_warn_on_fork=0 GLOG_vmodule=MemcachedClient=-1 \
 srun --mpi=pmi2 -p $1 -n$2 --gres=gpu:8 --ntasks-per-node=8 --job-name=${name} \
 python $ROOT/models/pytorch-object-detection/tools/train_val.py \
   --config=$cfg \
-  2>&1 | tee log/pod/train.{name}.log.$T
+  2>&1 | tee log/pod/train.${name}.log.$T
