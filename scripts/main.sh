3641002e502b2b206871fc1a9b736f92ca9660ce
wangqian4 <wangqian4@sensetime.com>
2019-11-07 14:38:15 +0800

    modify run_raw.sh; rename

--- scripts/main.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-/mnt/lustre/share/platform/dep/openmpi-2.1.6-cuda9.0/bin/mpirun \
-    --allow-run-as-root \
-    --hostfile ./mpirun_hostfile \
-    --np 2 \
-    --npernode 2 \
-    bash mpirun_main.sh \
-    resnet \
-    --max_epoch 10
---
+++ scripts/run_raw.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+NODE_NUM=1
+GPU_NUM=8
+MAX_EPOCH=120
+MODEL_NAME=resnet
+TOTAL_NUM=$((${NODE_NUM} * ${GPU_NUM}))
+
+export NVIDIA_DRIVER_CAPABILITIES="compute,utility"
+export NVIDIA_REQUIRE_CUDA="cuda>=8.0"
+export MV2_ENABLE_AFFINITY="0"
+
+/mnt/lustre/share/platform/dep/openmpi-2.1.6-cuda9.0/bin/mpirun \
+    --allow-run-as-root \
+    --hostfile ./mpirun_hostfile \
+    --np ${TOTAL_NUM} \
+    --npernode ${NODE_NUM} \
+    bash mpirun_main.sh \
+    ${MODEL_NAME} \
+    --max_epoch ${MAX_EPOCH}
