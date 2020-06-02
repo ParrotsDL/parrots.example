cd79fff43e4b07088ec7dd0d21636f8cbeddc8bc
zhangheng1 <zhangheng1@sensetime.com>
2020-06-02 10:59:56 +0800

    add mild
    Signed-off-by: zhangheng1 <zhangheng1@sensetime.com>

---
+++ runner/mild/train.sh
@@ -0,0 +1,19 @@
+partition=$1
+gpu_num=$2
+g=$(($2<8?$2:8))
+name=$3
+
+ROOT=.
+cfg=$ROOT/configs/mild/${name}.yaml
+
+# export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:$ROOT/models/mild/evaluate/kestrel
+export PYTHONPATH=$ROOT/models/mild:$ROOT/models/mild/thirdparty_update:$PYTHONPATH
+
+mkdir -p log/mild
+now=$(date +"%Y%m%d_%H%M%S")
+
+OMPI_MCA_mpi_warn_on_fork=0 GLOG_vmodule=MemcachedClient=-1 \
+srun --mpi=pmi2 -p $partition --job-name=${name} \
+    --gres=gpu:$g -n$gpu_num --ntasks-per-node=$g  \
+    python -u $ROOT/models/mild/run/start.py "cuda" "mimic" $cfg \
+    2>&1 | tee log/mild/train_${name}.log-$now
