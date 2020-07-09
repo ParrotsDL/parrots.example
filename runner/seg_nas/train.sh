b6c128e9be252dad9f09c40840572ac0e9fa5442
jiaomenglei <jiaomenglei@sensetime.com>
2020-07-09 09:10:09 +0800

    update

--- runner/seg_nas/train.sh
+++ runner/seg_nas/train.sh
@@ -24,18 +24,6 @@ case $name in
       PYTHON_ARGS="python -u models/Light_Nas_zpzhang/main.py \
          --config=configs/seg_nas/single_path_oneshot/config.benchmark.yaml \
          --step=$step"
-      ;; 
-    "single_path_oneshot_ceph_benchmark")
-      step=evolution
-      PYTHON_ARGS="python -u models/Light_Nas_zpzhang/main.py \
-         --config=configs/seg_nas/single_path_oneshot/config_ceph.benchmark.yaml \
-         --step=$step"
-      ;;
-    "single_path_oneshot_ceph")
-      step=evolution
-      PYTHON_ARGS="python -u models/Light_Nas_zpzhang/main.py \
-         --config=configs/seg_nas/single_path_oneshot/config_ceph.yaml \
-         --step=$step"
       ;;
     *)
       echo "invalid $name"
