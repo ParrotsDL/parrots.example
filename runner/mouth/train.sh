806281ccd53f360fdcb32da683f7cf9ef856e728
maxiaoming <maxiaoming@sensetime.com>
2020-07-16 17:37:55 +0800

    Upload New File

---
+++ runner/mouth/train.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+mkdir -p log/mouth/
+
+T=`date +%m%d%H%M`
+name=$3
+ROOT=.
+cfg=$ROOT/configs/mouth/${name}.yaml
+
+array=( $@ )
+len=${#array[@]}
+EXTRA_ARGS=${array[@]:3:$len}
+
+pyroot=$ROOT/models/mouth_inpaint_network
+export PYTHONPATH=$pyroot:$PYTHONPATH
+SRUN_ARGS=${SRUN_ARGS:-""}
+
+OMPI_MCA_mpi_warn_on_fork=0 GLOG_vmodule=MemcachedClient=-1 \
+srun --mpi=pmi2 -p $1 -n$2 --gres=gpu:8 --ntasks-per-node=8 --job-name=mouth_${name} ${SRUN_ARGS}\
+python $ROOT/models/mouth_inpaint_network/train.py \
+  $cfg  log/mouth/ ${EXTRA_ARGS} \
+  2>&1 | tee $ROOT/log/mouth/train.mouth_${name}.log.$T
+
