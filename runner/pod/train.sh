c7646ecb667c0dd11bb142d173af307fe473ebdb
jiaomenglei <jiaomenglei@sensetime.com>
2020-07-20 10:29:12 +0800

    fix --gres and --ntasks-per-node

--- runner/pod/train.sh
+++ runner/pod/train.sh
@@ -6,6 +6,7 @@ T=`date +%m%d%H%M`
 name=$3
 ROOT=.
 cfg=$ROOT/configs/pod/${name}.yaml
+g=$(($2<8?$2:8))
 
 array=( $@ )
 len=${#array[@]}
@@ -16,7 +17,7 @@ export PYTHONPATH=$pyroot:$PYTHONPATH
 SRUN_ARGS=${SRUN_ARGS:-""}
 
 OMPI_MCA_mpi_warn_on_fork=0 GLOG_vmodule=MemcachedClient=-1 \
-srun --mpi=pmi2 -p $1 -n$2 --gres=gpu:8 --ntasks-per-node=8 --job-name=pod_${name} ${SRUN_ARGS}\
+srun --mpi=pmi2 -p $1 -n$2 --gres gpu:$g --ntasks-per-node $g --job-name=pod_${name} ${SRUN_ARGS}\
 python $ROOT/models/pytorch-object-detection/tools/train_val.py \
   --config=$cfg ${EXTRA_ARGS} \
   2>&1 | tee $ROOT/log/pod/train.${name}.log.$T
