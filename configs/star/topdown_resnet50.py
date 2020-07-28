c3052412478603986312aa3b2c1cd5366379a2d1
hanyachao <hanyachao@sensetime.com>
2020-07-28 18:08:37 +0800

    update star

--- configs/star/topdown_resnet50.py
+++ configs/star/topdown_resnet50.py
@@ -137,7 +137,7 @@ train_cfg = dict(
     trainer=True,
     type='TrainTopDown',
 
-    batch_size_per_gpu=64,
+    batch_size_per_gpu=32,
     workers_per_gpu=2,
     shuffle=True,
 
@@ -161,7 +161,7 @@ test_cfg = dict(
     tester=True,
     type='TestTopDown',
 
-    batch_size_per_gpu=64,
+    batch_size_per_gpu=32,
     workers_per_gpu=2,
     coco_det_file='',
     coco_bbox_flip='pretrained_models/det/COCO_val2017_detections_AP_H_56_person.json',
