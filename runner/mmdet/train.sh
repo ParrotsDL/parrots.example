99e7495bc0003a997518d53184acc22932a4531c
yangruichao <yangruichao@sensetime.com>
2020-08-04 13:41:56 +0800

    add mmdet to parrots.test

--- runner/mmdet/train.sh
+++ runner/mmdet/train.sh
@@ -1,25 +1,24 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+mkdir -p log/mmdet
+
+T=`date +%m%d%H%M`
+name=$3
+ROOT=.
+EXTRA_ARGS=${@:4}
+
+pyroot=$ROOT/models/mmdet/tools
+export PYTHONPATH=$pyroot:$PYTHONPATH
+g=$(($2<8?$2:8))
+SRUN_ARGS=${SRUN_ARGS:-""}
+
+PYTHON_ARGS="python -u models/mmdet/tools/train.py --config=configs/mmdet/mask_rcnn/${name}.py --launcher=slurm"
 
 set -x
-ROOT=`pwd`
-pyroot=$ROOT/models/mmdetection-pro
-export PYTHONPATH=$pyroot:$PYTHONPATH
-
-PARTITION=$1
-GPUS=${2:-8}
-JOB_NAME=$3
-CONFIG=$ROOT/configs/mmdet/${JOB_NAME}.py
-WORK_DIR=mmdet/$3
-GPUS_PER_NODE=${GPUS_PER_NODE:-8}
-SRUN_ARGS=${SRUN_ARGS:-""}
-PY_ARGS=${PY_ARGS:-"--validate"}
-
-srun -p ${PARTITION} \
-    --job-name=${JOB_NAME} \
-    --gres=gpu:${GPUS_PER_NODE} \
-    --ntasks=${GPUS} \
-    --ntasks-per-node=${GPUS_PER_NODE} \
-    --kill-on-bad-exit=1 \
-    ${SRUN_ARGS} \
-    python -u ${pyroot}/tools/train.py ${CONFIG} --work_dir=${WORK_DIR} --launcher="slurm" ${PY_ARGS} \
-    2>&1
+OMPI_MCA_mpi_warn_on_fork=0 GLOG_vmodule=MemcachedClient=-1 \
+srun -K --mpi=pmi2 -p $1 -n$2 \
+        --gres gpu:$g \
+        --ntasks-per-node $g \
+        --job-name=mmdet_${name} ${SRUN_ARGS}\
+    $PYTHON_ARGS $EXTRA_ARGS \
+    2>&1 | tee $ROOT/log/mmdet/train.${name}.log.$T
