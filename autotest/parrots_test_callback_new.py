769cfa0fe2534950efc06a1da82d13033fcd2757
jiaomenglei <jiaoml1996@163.com>
2020-08-19 17:31:19 +0800

    update

--- autotest/parrots_test_callback_new.py
+++ autotest/parrots_test_callback_new.py
@@ -63,7 +63,7 @@ def after_callback_wrapper(config, run_type):
         pavi_ret[pk] = pv
 
     config.update(pavi_ret)
-    config['life'] = 1
+    config['test_life'] = 1
     print(yaml.dump(config))
 
 
@@ -142,7 +142,7 @@ def update_thresh_wrapper(config, framework, model_name, run_type):
     dump(org_config, config_path, file_format='yaml', default_flow_style=False)
 
     config = config[run_type]
-    config['life'] = 1
+    config['test_life'] = 1
     print(yaml.dump(config))
 
 
@@ -159,7 +159,7 @@ def pre_callback_wrapper(config, run_type):
             config = comm_table
     if 'placeholder' in config.keys():
         del config['placeholder']
-    config['life'] = 0
+    config['test_life'] = 0
     print(yaml.dump(config))
 
 
