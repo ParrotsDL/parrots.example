095f56cc09e91609c9beddcf633e872e78c8b268
jiaomenglei <jiaoml1996@163.com>
2020-09-01 11:32:00 +0800

    update

--- autotest/parrots_test_callback_new.py
+++ autotest/parrots_test_callback_new.py
@@ -9,9 +9,14 @@ import warnings
 
 
 import psutil
+try:
+    from autoparrots.utils.log import LOG as logger
+except:
+    logger = None
 from autoparrots.utils.fileio import dump
 from autoparrots.command.entry import trace_up
 from autoparrots.command.task import kill_task
+from autoparrots.schedulers import load_taskinfo
 
 # 公共表
 comm_table = {
@@ -69,6 +74,7 @@ def _watch_for_kill_time_limited(framework, model, config, time_limited_flag='[E
     time.sleep(60)
     # wait for job_pid, job_log_path
     job_pid = None
+    slurm_job_id = None
     job_log_path = None
     workdir = None
     name = None
@@ -77,34 +83,50 @@ def _watch_for_kill_time_limited(framework, model, config, time_limited_flag='[E
     while True:
         time.sleep(30)
         interval_time = time.time() - start_time
-        # break if task_yaml_path don't exist for 10 minutes
         if ((not job_pid or
              not job_log_path or
              not workdir or
-             not name) and
+             not name or
+             not slurm_job_id) and
              interval_time >= job_wait_to_run_time_thresh * 60 * 60):
             break
-
+        
+        # get job pid
         try:
             job_pid = int(os.environ['pid'])
         except Exception:
             job_pid = None
+        # get job wait_to_run_time_thresh
         try:
             job_wait_to_run_time_thresh = int(os.environ['wait_to_run_time_thresh'])
         except Exception:
             job_wait_to_run_time_thresh = 1
+        # get job log path
         job_log_path = os.environ['log_path']
+        # get job workdir
         try:
             workdir = os.environ['workdir']
             workdir = os.sep.join(workdir.split(os.sep)[:-2])
         except:
             workdir = None
+        # get job name
         name = os.environ['name']
-        if job_pid and job_log_path and workdir and name:
+        # get slurm_job_id
+        if workdir and name:
+            info = load_taskinfo(workdir)
+            job_names = list(filter(lambda j: j['name'] in [name], info['jobs']))
+            if len(job_names) >= 0:
+                job_info = job_names[0]
+                try:
+                    slurm_job_id = int(job_info['slurm_job_id'])
+                except Exception:
+                    slurm_job_id = None
+        if job_pid and job_log_path and workdir and name and slurm_job_id:
             break
         # break if job_pid is die.
         if job_pid and (not psutil.pid_exists(job_pid)):
             break
+
     # monitor log
     last_lines_hash = None
     last_lines_hash_start_time = time.time()
@@ -113,7 +135,8 @@ def _watch_for_kill_time_limited(framework, model, config, time_limited_flag='[E
         if (not job_pid or
             not job_log_path or
             not workdir or
-            not name):
+            not name or
+            not slurm_job_id):
             break
         time.sleep(60)
         is_time_limit = False
@@ -128,6 +151,9 @@ def _watch_for_kill_time_limited(framework, model, config, time_limited_flag='[E
             if time.time() - last_lines_hash_start_time >= wait_time_log_no_change * 60:
                 kill_task(workdir, [name])
                 is_time_limit = True
+                if logger:
+                    logger.error("Job({})[pid: {}, slurm: {}] is killed because the log has not changed for {} minutes.".format(
+                        name, job_pid, slurm_job_id, wait_time_log_no_change))
         else:
             last_lines_hash = lines_hash
             last_lines_hash_start_time = time.time()
@@ -142,6 +168,9 @@ def _watch_for_kill_time_limited(framework, model, config, time_limited_flag='[E
                 if time.time() - time_limited_start_time >= wait_time_occur_time_limited * 60:
                         kill_task(workdir, [name])
                         is_time_limit = True
+                        if logger:
+                            logger.error("Job({})[pid: {}, slurm: {}] is killed because the log occurs '{}' for {} minutes.".format(
+                                name, job_pid, slurm_job_id, time_limited_flag, wait_time_occur_time_limited))
             else:
                 time_limited_start_time = time.time()
 
@@ -151,6 +180,11 @@ def _watch_for_kill_time_limited(framework, model, config, time_limited_flag='[E
         # break if job_pid is die.
         if not psutil.pid_exists(job_pid):
             break
+
+    if logger:
+        logger.error("Job({})[pid: {}, slurm: {}] The child process monitoring the log has exited, \
+                      with [job_pid: {}, job_log_path: {}, workdir: {}, name: {}, slurm_job_id: {}]".format(
+            name, job_pid, slurm_job_id, job_pid, job_log_path, workdir, name, slurm_job_id))
 
 def after_callback_wrapper(config, value_type, run_type):
     if run_type in config.keys():
