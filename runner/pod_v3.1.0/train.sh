f5ac2843b1db7860d26f32f36a8e43603c8939ba
Zhu Juan <zhujuan1@sensetime.com>
2020-10-09 17:55:36 +0800

    fix srun args

--- runner/pod_v3.1.0/train.sh
+++ runner/pod_v3.1.0/train.sh
@@ -17,7 +17,7 @@ export PYTHONPATH=$pyroot:$PYTHONPATH
 SRUN_ARGS=${SRUN_ARGS:-""}
 
 OMPI_MCA_mpi_warn_on2fork=0 GLOG_vmodule=MemcachedClient=-1 \
-PYTORCH_VERSION=1.3 srun --mpi=pmi2 -p $1 -n$2 --gres gpu:$g --ntasks-per-node $g --job-name=pod_${name} ${SRUN_ARGS}\
+PYTORCH_VERSION=1.3 srun --mpi=pmi2 -p $1 -n$2 --gres gpu:$g --ntasks-per-node $g --job-name=pod_v3.1.0_${name} ${SRUN_ARGS} \
 python -m pod train \
   --config=$cfg ${EXTRA_ARGS} \
   2>&1 | tee $ROOT/log/pod_v3.1.0/train.${name}.log.$T
