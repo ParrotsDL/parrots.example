20ec3a68655f5af7104f2df4f683413cc63f413d
maxiaoming <maxiaoming@sensetime.com>
2020-10-27 00:53:30 +0800

    Update gcnet_r101-d8_512x1024_40k_cityscapes.py

--- configs/mmseg/gcnet_r101-d8_512x1024_40k_cityscapes.py
+++ configs/mmseg/gcnet_r101-d8_512x1024_40k_cityscapes.py
@@ -1,4 +1,4 @@
 _base_ = './gcnet_r50-d8_512x1024_40k_cityscapes.py'
-model = dict(pretrained='/mnt/lustre/share_data/xujiarui/pretrain_model/resnet101_v1c_trick-e67eebb6.pth', backbone=dict(depth=101))
+model = dict(pretrained='/mnt/lustre/share_data/maxiaoming/mmseg_pretrain_model/resnet101_v1c_trick-e67eebb6.pth', backbone=dict(depth=101))
 
 #open-mmlab://resnet101_v1c'
