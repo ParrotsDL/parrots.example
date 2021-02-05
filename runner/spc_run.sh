8aa50258c34d7b9008982de8530ecbf474548520
wuwenli1 <wuwenli1@sensetime.com>
2021-02-05 16:07:24 +0800

    update mmediting for container

--- runner/spc_run.sh
+++ runner/spc_run.sh
@@ -13,13 +13,19 @@ EXTRA_ARGS=${array[@]:4:$len}
 cp /home/${USER}/petreloss.conf /mnt/lustre/${USER}/petreloss.conf
 cp -r /home/${USER}/.pavi /mnt/lustre/${USER}/.pavi
 
-current=`date "+%Y-%m-%d %H:%M:%S"`
-timeStamp=`date -d "$current" +%s`
-currentTimeStamp=$((timeStamp*1000+10#`date "+%N"`/1000000))
-JOB_NAME=${currentTimeStamp}
+if [ -z ${container_job_name} ];then 
+        current=`date "+%Y-%m-%d %H:%M:%S"`
+        timeStamp=`date -d "$current" +%s`
+        currentTimeStamp=$((timeStamp*1000+10#`date "+%N"`/1000000))
+        JOB_NAME=${currentTimeStamp}
+else
+        JOB_NAME=${container_job_name}
+fi
+
  
 PARTITION=${NAMESPACE}
 IMAGE="registry.sensetime.com/parrots/parrots:pat_latest"   #镜像名称可能也需要search_config.yaml中指定
+
 ## 资源信息
 NODES=$((${GPUS}<=8?1:2))
 GPU_PER_NODE=$((${GPUS}>8?8:${GPUS}))       ## 每个节点GPU的计算方法可能也是一个问题，因为有的机器运行着开发机，所以每个节点可能占不到8个GPU
