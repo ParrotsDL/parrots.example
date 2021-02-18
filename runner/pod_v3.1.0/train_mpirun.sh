9fc7bbef2de95cdff0b256d861a4229e3733b52e
zhujuan1 <zhujuan1@sensetime.com>
2021-02-18 15:51:39 +0800

    pod3.1 for phoenix

---
+++ runner/pod_v3.1.0/train_mpirun.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+source /usr/local/env/pat_latest
+mkdir -p log/pod_v3.1.0/
+
+T=`date +%m%d%H%M%S`
+name=$1
+ROOT=.
+cfg=$ROOT/configs/pod_v3.1.0/${name}.yaml
+
+array=( $@ )
+len=${#array[@]}
+EXTRA_ARGS=${array[@]:1:$len}
+
+if [ "x$OMPI_COMM_WORLD_LOCAL_RANK" == "x0" ]
+then
+    cp /mnt/lustre/${USER}/petreloss.conf /home/${USER}/petreloss.conf
+    cp -r /mnt/lustre/${USER}/.pavi /home/${USER}/.pavi
+fi
+
+pyroot=$ROOT/models/pytorch-object-detection-v3.1.0
+export PYTHONPATH=$pyroot:$PYTHONPATH
+export PYTORCH_VERSION=1.3
+
+
+python -m pod train \
+  --config=$cfg ${EXTRA_ARGS} \
+  2>&1 | tee $ROOT/log/pod_v3.1.0/train.${name}.log.$T
