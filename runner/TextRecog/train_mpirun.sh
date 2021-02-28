49a25531339152e28004b8f246016d778e30100d
zhujuan1 <zhujuan1@sensetime.com>
2021-02-28 23:16:14 +0800

    remove textrecog compile

--- runner/TextRecog/train_mpirun.sh
+++ runner/TextRecog/train_mpirun.sh
@@ -9,15 +9,6 @@ array=( $@ )
 len=${#array[@]}
 EXTRA_ARGS=${array[@]:1:$len}
 
-if [ ! -d "models/TextRecog/pytorch-ctc/pytorch-ctc-0.3.2/build" ]; then
-    sh ./setup.sh
-else
-    ext_path="models/TextRecog/pytorch-ctc/pytorch-ctc-0.3.2/parrots_binding/warp_ctc_pat.cpython-36m-x86_64-linux-gnu.so"
-    while [ ! -f "${ext_path}" ];
-    do
-        sleep 5
-    done
-fi
 
 ## 下面是未定的存储和pavi使用方案，我先暂时这样写了
 if [ $OMPI_COMM_WORLD_LOCAL_RANK == '0' ]
@@ -36,21 +27,6 @@ cfg=$ROOT/configs/TextRecog/${name}_bn1.yaml
 pyroot=$ROOT/models/TextRecog
 export PYTHONPATH=$pyroot:$PYTHONPATH
 
-if [ $OMPI_COMM_WORLD_LOCAL_RANK == '0' ]
-then
-    cd models/TextRecog
-    cd pytorch-ctc/pytorch-ctc-0.3.2
-    mkdir build
-    cd build
-    cmake ..
-    make
-    export WARP_CTC_PATH=`pwd`
-    cd ../parrots_binding
-    pip install -v -e . --user
-    cd ../../../../../
-fi
-
-export PYTHONPATH=models/TextRecog/pytorch-ctc/pytorch-ctc-0.3.2/parrots_binding:$PYTHONPATH
 
 python -u models/TextRecog/tools/train_val.py \
   --config=$cfg --phase train --launcher mpi  ${EXTRA_ARGS} \
