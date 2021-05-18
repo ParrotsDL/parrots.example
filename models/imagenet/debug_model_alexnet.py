f5cf1163909581f39cd20934f92a85a9ee8c85bc
fengsibo <fengsibo@sensetime.com>
2021-05-18 20:19:43 +0800

    alexnet

--- models/imagenet/debug_model_alexnet.py
+++ models/imagenet/debug_model_alexnet.py
@@ -57,10 +57,54 @@ def hook(name, mm, tag='forward'):
         #  print("Layer {} {}:\n input {}\n output {}\n".format(name, mm, tag, get_data2(input), get_data2(output)))
     return inner_hook
 
+class AlexNet(nn.Module):
+
+    def __init__(self, num_classes = 1000):
+        super(AlexNet, self).__init__()
+        self.features = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=11, stride=4, padding=2),
+            nn.ReLU(inplace=False),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 192, kernel_size=5, padding=2),
+            nn.ReLU(inplace=False),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(192, 384, kernel_size=3, padding=1),
+            nn.ReLU(inplace=False),
+            nn.Conv2d(384, 256, kernel_size=3, padding=1),
+            nn.ReLU(inplace=False),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            # nn.ReLU(inplace=False),
+            # nn.MaxPool2d(kernel_size=3, stride=2),
+        )
+        self.avgpool = nn.AdaptiveAvgPool2d((6, 6))
+        self.classifier = nn.Sequential(
+            # nn.Dropout(),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=False),
+            # nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=False),
+            nn.Linear(4096, num_classes),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = self.avgpool(x)
+        if torch.__version__ == "parrots":
+            x = x.cpu()#.contiguous()
+            # #x = nn.functional.adaptive_avg_pool2d(x, (6,6))
+            x = torch.flatten(x, 1)
+            x = x.cuda()
+        else:
+            x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
+
 if __name__=="__main__":
     
     # m = models.vgg_min.vgg16()
-    m = models.alexnet()
+    # m = models.alexnet()
+    m = AlexNet()
 
     input = torch.randn(2, 3, 224, 224, requires_grad=True)
     
