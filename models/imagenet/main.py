af3eef451c1f1003bfd78d8cafbbaf63d38f30a6
zhanglupei1 <zhanglupei1@sensetime.com>
2021-05-28 15:05:03 +0800

    fix ip-get in diff slurm

--- models/imagenet/main.py
+++ models/imagenet/main.py
@@ -60,9 +60,10 @@ def main():
         args.rank = int(os.environ['SLURM_PROCID'])
         args.world_size = int(os.environ['SLURM_NTASKS'])
         args.local_rank = int(os.environ['SLURM_LOCALID'])
-        node_list = str(os.environ['SLURM_NODELIST'])
-        node_parts = re.findall('[0-9]+', node_list)
-        os.environ['MASTER_ADDR'] = f'{node_parts[0]}.{node_parts[1]}.{node_parts[2]}.{node_parts[3]}'
+        #node_list = str(os.environ['SLURM_NODELIST'])
+        #node_parts = re.findall('[0-9]+', node_list)
+        #os.environ['MASTER_ADDR'] = f'{node_parts[-4]}.{node_parts[-3]}.{node_parts[-2]}.{node_parts[-1]}'
+        os.environ['MASTER_ADDR'] = socket.gethostbyname(socket.gethostname()) 
         os.environ['MASTER_PORT'] = str(args.port)
     else:
         args.rank = int(os.environ['OMPI_COMM_WORLD_RANK'])
