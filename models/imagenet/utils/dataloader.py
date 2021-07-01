8ec1234b99130fba294f915a73e4ccaea3164101
mamengping <mamengping@sensetime.com>
2021-07-01 15:43:01 +0800

    example dummydataset

--- models/imagenet/utils/dataloader.py
+++ models/imagenet/utils/dataloader.py
@@ -1,3 +1,4 @@
+import copy
 import io
 import torchvision.transforms as transforms
 from torch.utils.data import Dataset
@@ -31,16 +32,23 @@ class CephDataset(Dataset):
         if self.meta_list[-1] == '':
             self.meta_list.pop()
         self.num = len(self.meta_list)
+        self.ddt_flag = 0
+        self.ddt_img = 0
 
     def __len__(self):
         return self.num
 
     def __getitem__(self, index):
-        filename = self.image_dir + self.meta_list[index].split()[0]
-        cls = int(self.meta_list[index].split()[1])
+        if self.ddt_flag:
+            filename = self.image_dir + self.meta_list[index].split()[0]
+            cls = int(self.meta_list[index].split()[1])
 
-        img = Image.open(io.BytesIO(self.client.Get(filename, update_cache=True)))
-        img = img.convert('RGB')
+            img = Image.open(io.BytesIO(self.client.Get(filename, update_cache=True)))
+            img = img.convert('RGB')
+        else:
+            img = copy.deepcopy(self.ddt_img)
+            cls = int(self.meta_list[index].split()[1])
+            print("dummydataset")
 
         # transform
         if self.transform is not None:
