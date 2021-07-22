04aff61ba07db4d13753f1a5be13526059bd6362
wuwenli <wuwenli1@sensetime.com>
2021-07-22 16:44:01 +0800

    modify update v5

--- models/imagenet/main.py
+++ models/imagenet/main.py
@@ -26,7 +26,8 @@ from utils.dataloader import build_dataloader
 from utils.misc import accuracy, check_keys, AverageMeter, ProgressMeter
 from utils.loss import LabelSmoothLoss
 import math
-from algolib.utils import add_argument, logger_pavi, benchmark_data
+
+from algolib.utils import add_argument, benchmark_data, logger_pavi
 
 parser = argparse.ArgumentParser(description='ImageNet Training Example')
 parser.add_argument('--config',
@@ -53,7 +54,7 @@ add_argument(parser,
             cfg_name=os.path.join(
                  os.path.abspath(__file__).rsplit('/', 1)[0],
                  '../../algolib/runner/example.yaml'),
-            model_name=os.getenv('name'))
+            model_name=os.getenv('MODEL_NAME'))
 
 
 logging.basicConfig(format='%(asctime)s %(levelname)s %(message)s')
@@ -266,20 +267,6 @@ def main():
     logger_pavi(end_time - start_time, end_time - run_time,
                 np.mean(iter_time_list), mem_alloc, mem_cached, args.benchmark,
                 logger, monitor_writer, args.rank)
-    # if args.benchmark and args.rank == 0:
-    #     logger.info('__benchmark_total_time(h): {}'.format((end_time - start_time) / 3600))
-    #     logger.info('__benchmark_pure_training_time(h): {}'.format((end_time - run_time) / 3600))
-    #     logger.info('__benchmark_avg_iter_time(s): {}'.format(np.mean(iter_time_list)))
-    #     logger.info('__benchmark_mem_alloc(mb): {}'.format(mem_alloc))
-    #     logger.info('__benchmark_mem_cached(mb): {}'.format(mem_cached))
-
-    # if args.benchmark and args.rank == 0 and monitor_writer:
-    #     monitor_writer.add_scalar('__benchmark_total_time(h)',(end_time - start_time) / 3600,1)
-    #     monitor_writer.add_scalar('__benchmark_pure_training_time(h)',(end_time - run_time) / 3600,1)
-    #     monitor_writer.add_scalar('__benchmark_avg_iter_time(s)',np.mean(iter_time_list),1)
-    #     monitor_writer.add_scalar('__benchmark_mem_alloc(mb)',mem_alloc,1)
-    #     monitor_writer.add_scalar('__benchmark_mem_cached(mb)',mem_cached,1)
-    #     monitor_writer.add_snapshot('__benchmark_pseudo_snapshot', None, 1)
 
 
 def train(train_loader, model, criterion, optimizer, epoch, args,
@@ -348,26 +335,7 @@ def train(train_loader, model, criterion, optimizer, epoch, args,
         iter_end_time = time.time()
         iter_start_time = time.time()
         benchmark_data(i, iter_end_time - iter_start_time, len(train_loader),
-                       args.benchmark, iter_time_list)
-
-        # #TODO(just do once)
-        # if args.benchmark is not None:
-        #     if args.benchmark == 'ON' or args.benchmark == 'True' or args.benchmark == '1':
-        #         left_limit = math.floor(1/3*len(train_loader))
-        #         right_limit = math.floor(2/3*len(train_loader))
-        #         limit_range = right_limit - left_limit
-        #     elif ',' in args.benchmark:
-        #         iter_range = [int(iter) for iter in
-        #                     args.benchmark.split(",")]
-        #         left_limit = iter_range[0]
-        #         right_limit = iter_range[1]
-        #         limit_range = right_limit - left_limit
-        # else:
-        #     limit_range = None
-
-        # if limit_range and len(iter_time_list) <= limit_range and \
-        #    i >= left_limit and i <= right_limit:
-        #     iter_time_list.append(iter_end_time - iter_start_time)
+                       args.benchmark)
 
         if i % args.log_freq == 0:
             progress.display(i)
