6cf80cd2148da208e0a8386139166df9a530bc0d
wuwenli <wuwenli1@sensetime.com>
2021-10-25 22:48:30 +0800

    Wwl/example sub2

--- models/imagenet/main.py
+++ models/imagenet/main.py
@@ -432,7 +432,7 @@ def test(test_loader, model, criterion, args):
 if __name__ == '__main__':
     # parrots.algolib
     try:
-        from algolib.common import init
+        from submodules.common import init
         init(os.path.join(
             os.path.abspath(__file__).rsplit('/', 1)[0],
             '../../algolib/runner/example.yaml'),
