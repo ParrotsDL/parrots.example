2ed3dd137a0870b56f085cf6aedd4c3323234e72
shixuan <shixuan@sensetime.com>
2021-11-05 13:31:21 +0800

    support command help in shell script

--- models/imagenet/train_mlu.sh
+++ models/imagenet/train_mlu.sh
@@ -1,9 +1,27 @@
 mkdir -p log
 now=$(date +"%Y%m%d_%H%M%S")
 
+function helper
+{
+    echo "Command:"
+    echo "  sh train_mlu.sh [0] [1] [2]"
+    echo "  args:"
+    echo "      [0] model name"
+    echo "      [1] partition name"
+    echo "      [2] card number"
+    echo "  eg. sh train_mlu.sh googlenet camb_mlu290 8"
+    echo "      which means training googlenet model on camb_mlu290 partition with 8 cards."
+}
+
 model=$1
 partition=$2
 gpus=$3
+
+if [ $# -ne 3 ]; then
+    echo "[ERROR] need 3 arguments."
+    helper
+    exit 1
+fi
 
 cfg=configs/${model}.yaml
 jobname=${model}_example
@@ -11,4 +29,10 @@ g=$(($gpus<8?$gpus:8))
 
 srun -p $partition --job-name=$jobname \
     --gres=mlu:$g -n$gpus python -u main.py --config $cfg \
-    2>&1 | tee log/train_${jobname}_${gpus}cards.log-$now
+    2>&1 | tee log/train_${jobname}.log-$now
+
+if [ ${PIPESTATUS[0]} -eq 1 ]; then
+    echo "[ERROR] training failed."
+    helper
+    exit 1
+fi
