92d2580679dd7e3469e091be2c1f109fd9835d28
shixuan <shixuan@sensetime.com>
2021-11-16 10:17:42 +0800

    fix avg

--- models/imagenet/main.py
+++ models/imagenet/main.py
@@ -422,10 +422,11 @@ def test(test_loader, model, criterion, args):
                     [loss.item(), acc1[0].item(), acc5[0].item()]).float().cuda()
             if args.dist:
                 dist.all_reduce(stats_all)
+            stats_all /= args.world_size
             
             losses.update(stats_all[0].item())
-            top1.update(stats_all[1].item())
-            top5.update(stats_all[2].item())
+            top1.update(stats_all[1].item() * 100 / target.size(0))
+            top5.update(stats_all[2].item() * 100 / target.size(0))
 
             # measure elapsed time
             batch_time.update(time.time() - end)
@@ -443,14 +444,11 @@ def test(test_loader, model, criterion, args):
                 args.rank, loss_each, acc1_each, raw_stats[1].item(), total_size,
                 acc5_each, raw_stats[2].item(), total_size))
 
-        loss_avg = losses.avg / args.world_size
-        top1_avg = top1.sum * 100 / (total_size * args.world_size)
-        top5_avg = top5.sum * 100 / (total_size * args.world_size)
         logger.info(
             ' * All Loss {:.4f} Acc@1 {:.3f} Acc@5 {:.3f}'.format(
-                loss_avg, top1_avg, top5_avg))
+                losses.avg, top1.avg, top5.avg))
 
-    return loss_avg, top1_avg, top5_avg
+    return losses.avg, top1.avg, top5.avg
 
 
 if __name__ == '__main__':
