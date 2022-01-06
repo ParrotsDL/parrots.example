b5df50e836f0a17fa960f92d0f468b9434a51528
hanlei <hanlei@sensetime.com>
2022-01-06 09:16:13 +0000

    add coco2017 config and test script

---
+++ tools/test_mlu.sh
@@ -0,0 +1,21 @@
+#/bin/sh
+if [ ! -d "logs" ]; then
+  mkdir logs
+fi
+T=`date +%m%d%H%M%S`
+CUR_DIR=$(cd $(dirname "$pwd");pwd)
+
+log_path=${CUR_DIR}/logs
+batch_size=32
+data=data/coco2014.yaml
+
+# extra param of training
+array=( $@ )
+len=${#array[@]}
+EXTRA_ARGS=${array[@]:3:$len}
+
+srun -p $1 -n$2 --gres=mlu:$2 --ntasks-per-node=$2 \
+--job-name=yolov3 python tools/train.py \
+--data ${data} --batch-size ${batch_size} \
+--noval --test --pretrained_model $3 ${EXTRA_ARGS} \
+2>&1 | tee ${log_path}/yolov3_mlu290_test_${T}.log
