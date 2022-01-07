5366f8107796b91cb499b5a5bbad51af887ad0c7
SunXiaoye <sunxiaoye@sensetime.com>
2022-01-07 20:51:08 +0800

    Change the path to save log and checkpoint.

--- algolib/runner/train.sh
+++ algolib/runner/train.sh
@@ -37,19 +37,22 @@ EXTRA_ARGS=${array[@]:3:$len}
 SRUN_ARGS=${SRUN_ARGS:-""}
 
 # 5. model choice
+mkdir -p algolib_gen/example/${MODEL_NAME}/
 export PARROTS_DEFAULT_LOGGER=FALSE
 if [[ $3 =~ "sync" ]]; then
     PARROTS_EXEC_MODE=SYNC OMPI_MCA_mpi_warn_on_fork=0 GLOG_vmodule=MemcachedClient=-1 \
     srun --mpi=pmi2 -p $partition --job-name=example_${MODEL_MODEL_NAME} \
         --gres=gpu:$g -n$2 --ntasks-per-node=$g  ${SRUN_ARGS} \
         python -u $pyroot/models/imagenet/main.py --config ${cfg} \
+        --save_path=algolib_gen/example/${MODEL_NAME} \
         ${EXTRA_ARGS} \
-        2>&1 | tee algolib_gen/example/train_${MODEL_NAME}.log-$now
+        2>&1 | tee algolib_gen/example/${MODEL_NAME}/train_${MODEL_NAME}.log-$now
 else
     OMPI_MCA_mpi_warn_on_fork=0 GLOG_vmodule=MemcachedClient=-1 \
     srun --mpi=pmi2 -p $partition --job-name=example_${MODEL_NAME} \
         --gres=gpu:$g -n$2 --ntasks-per-node=$g  ${SRUN_ARGS}  \
         python -u $pyroot/models/imagenet/main.py --config ${cfg} \
+        --save_path=algolib_gen/example/${MODEL_NAME} \
         ${EXTRA_ARGS} \
-        2>&1 | tee algolib_gen/example/train_${MODEL_NAME}.log-$now
+        2>&1 | tee algolib_gen/example/${MODEL_NAME}/train_${MODEL_NAME}.log-$now
 fi
