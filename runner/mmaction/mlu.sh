55a51307b7bb6f4b9a914e86d39340ad667eae17
xieyuming1 <xieyuming1@sensetime.com>
2022-03-07 07:01:27 +0000

    add merged mlu test

---
+++ runner/mmaction/mlu.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+mkdir -p log/mmaction
+
+T=`date +%m%d%H%M%S`
+rgb_config="tsn_r50_1x1x3_80e_ucf101_rgb"
+flow_config="tsn_r50_1x1x3_30e_ucf101_flow"
+ROOT=.
+
+pyroot=$ROOT/models/mmaction
+export PYTHONPATH=$pyroot:$PYTHONPATH
+g=$(($2<8?$2:8))
+SRUN_ARGS=${SRUN_ARGS:-""}
+
+# 避免mm系列重复打印
+export PARROTS_DEFAULT_LOGGER=FALSE
+set -x
+
+# train rgb
+PYTHON_ARGS="python -u models/mmaction/tools/train.py --config configs/mmaction/${rgb_config}.py --launcher=slurm --validate"
+
+srun -p $1 -n$2 --gres mlu:$g --ntasks-per-node $g --job-name=mmaction_${rgb_config} ${SRUN_ARGS} \
+    $PYTHON_ARGS \
+    2>&1 | tee $ROOT/log/mmaction/train.${rgb_config}.log.$T
+
+# train flow
+PYTHON_ARGS="python -u models/mmaction/tools/train.py --config configs/mmaction/${flow_config}.py --launcher=slurm --validate"
+
+srun -p $1 -n$2 --gres mlu:$g --ntasks-per-node $g --job-name=mmaction_${flow_config} ${SRUN_ARGS} \
+    $PYTHON_ARGS \
+    2>&1 | tee $ROOT/log/mmaction/train.${flow_config}.log.$T
+
+
+# test rgb
+PYTHON_ARGS="python -u models/mmaction/tools/test.py --config configs/mmaction/${rgb_config}.py --launcher=slurm --checkpoint work_dirs/tsn_r50_1x1x3_75e_ucf101_split_1_rgb/latest.pth --out work_dirs/tsn_r50_1x1x3_75e_ucf101_split_1_rgb/rgb.pkl --eval top_k_accuracy"
+
+srun -p $1 -n$2 --gres mlu:$g --ntasks-per-node $g --job-name=mmaction_${rgb_config} ${SRUN_ARGS} \
+    $PYTHON_ARGS \
+    2>&1 | tee $ROOT/log/mmaction/test.${rgb_config}.log.$T
+
+# test flow
+PYTHON_ARGS="python -u models/mmaction/tools/test.py --config configs/mmaction/${flow_config}.py --launcher=slurm --checkpoint work_dirs/tsn_r50_1x1x3_75e_ucf101_split_1_flow/latest.pth --out work_dirs/tsn_r50_1x1x3_75e_ucf101_split_1_flow/flow.pkl --eval top_k_accuracy"
+
+srun -p $1 -n$2 --gres mlu:$g --ntasks-per-node $g --job-name=mmaction_${flow_config} ${SRUN_ARGS} \
+    $PYTHON_ARGS \
+    2>&1 | tee $ROOT/log/mmaction/test.${flow_config}.log.$T
+
+# merge result
+PYTHON_ARGS="python -u models/mmaction/tools/analysis/report_accuracy.py --scores work_dirs/tsn_r50_1x1x3_75e_ucf101_split_1_rgb/rgb.pkl work_dirs/tsn_r50_1x1x3_75e_ucf101_split_1_flow/flow.pkl --datalist /mnt/lustre/share/openmmlab/datasets/action/ucf101/ucf101_val_split_1_rawframes.txt"
+
+srun -p 1 -n 1 --gres mlu:1 --ntasks-per-node 1  --job-name=mmaction_merge ${SRUN_ARGS} \
+    $PYTHON_ARGS \
+    2>&1 | tee $ROOT/log/mmaction/merge_result.log.$T
