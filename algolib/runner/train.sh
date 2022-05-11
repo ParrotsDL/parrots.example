5760ba36171c6f5f4ea21868871cf43c9e65f69f
lhl <lihaolun.vendor@sensetime.com>
2022-05-11 17:15:48 +0800

    add

--- algolib/runner/train.sh
+++ algolib/runner/train.sh
@@ -1,41 +1,40 @@
+#!/bin/bash
 set -x
 set -o pipefail
 set -e
 
+# 0. check the most important SMART_ROOT
+echo  "!!!!!SMART_ROOT is" $SMART_ROOT
 if $SMART_ROOT; then
     echo "SMART_ROOT is None,Please set SMART_ROOT"
     exit 0
 fi
 
-if [ -x "$SMART_ROOT/submodules" ];then
-    submodules_root=$SMART_ROOT
-else    
-    submodules_root=$PWD
+# 1. set env_path
+if [[ $PWD =~ "example" ]]
+then 
+    pyroot=$PWD
+else
+    pyroot=$PWD/example
 fi
+echo $pyroot
 
-# 1. build file folder for save log,format: algolib_gen/frame
+# 2. build file folder for save log and set time
 mkdir -p algolib_gen/example
-
-# 2. set time
 now=$(date +"%Y%m%d_%H%M%S")
 
-# 3. set env
-path=$PWD
-if [[ "$path" =~ "submodules" ]]
-then 
-    pyroot=$submodules_root/example
-else
-    pyroot=$submodules_root/submodules/example
-fi
+# 3. set env variables
 export FRAME_NAME=example # customize for each frame
 export MODEL_NAME=$3
-cfg=$pyroot/algolib/configs/${MODEL_NAME}.yaml
 export PYTHONPATH=$pyroot:$PYTHONPATH
+export PARROTS_DEFAULT_LOGGER=FALSE
 
-# init_path
+# 4. init_path
+export PYTHONPATH=${SMART_ROOT}:$PYTHONPATH
 export PYTHONPATH=$SMART_ROOT/common/sites/:$PYTHONPATH # necessary for init
 
-# 4. build necessary parameter
+# 5. build necessary parameter
+cfg=$pyroot/algolib/configs/${MODEL_NAME}.yaml
 partition=$1
 g=$(($2<8?$2:8))
 array=( $@ )
@@ -43,11 +42,11 @@ len=${#array[@]}
 EXTRA_ARGS=${array[@]:3:$len}
 SRUN_ARGS=${SRUN_ARGS:-""}
 
+# 6. set port and choice model
 port=`expr $RANDOM % 10000 + 20000`
-
-# 5. model choice
 mkdir -p algolib_gen/example/${MODEL_NAME}/
-export PARROTS_DEFAULT_LOGGER=FALSE
+
+# 7. run model
 if [[ $3 =~ "sync" ]]; then
     PARROTS_EXEC_MODE=SYNC OMPI_MCA_mpi_warn_on_fork=0 GLOG_vmodule=MemcachedClient=-1 \
     srun --mpi=pmi2 -p $partition --job-name=example_${MODEL_MODEL_NAME} \
