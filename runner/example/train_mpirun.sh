6fab3043ac03c9dce0746ee283079f424489201d
sunxiaoye <sunxiaaoye@sensetime.com>
2022-11-25 15:31:26 +0800

    2day camb test for lab mpirun

--- runner/example/train_mpirun.sh
+++ runner/example/train_mpirun.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -x
-source $1
+#source $1
 
 name=$2
 array=( $@ )
 len=${#array[@]}
 EXTRA_ARGS=${array[@]:2:$len}
- 
+
 ## 下面是未定的存储和pavi使用方案，我先暂时这样写了
-if [ "x$OMPI_COMM_WORLD_LOCAL_RANK" == "x0" ]
-then
-    cp /mnt/lustre/${USER}/petreloss.conf /home/${USER}/petreloss.conf
-    cp -r /mnt/lustre/${USER}/.pavi /home/${USER}/.pavi
-fi
+#if [ "x$OMPI_COMM_WORLD_LOCAL_RANK" == "x0" ]
+#then
+#    cp /mnt/lustre/${USER}/petreloss.conf /home/${USER}/petreloss.conf
+#    cp -r /mnt/lustre/${USER}/.pavi /home/${USER}/.pavi
+#fi
 
 ROOT=.
 
@@ -21,10 +21,12 @@ mkdir -p log/example/
 now=$(date +"%Y%m%d_%H%M%S")
 
 cfg=$ROOT/configs/example/${name}.yaml
- 
+
 pyroot=$ROOT/models/parrots.example
 export PYTHONPATH=${pyroot}/code/:$PYTHONPATH
- 
-python -u $ROOT/models/parrots.example/models/imagenet/main.py --config ${cfg} \
-        ${EXTRA_ARGS} \
+
+port=`expr $RANDOM % 10000 + 10000`
+
+mpirun -np $1 python -u $ROOT/models/parrots.example/models/imagenet/main.py --config ${cfg} \
+        --launcher mpi --port $port  ${EXTRA_ARGS} \
         2>&1 | tee log/example/train_${name}.log-$now
---
+++ runner/transformer/train_mpi.sh
@@ -0,0 +1,46 @@
+mkdir -p log
+now=$(date +"%Y%m%d_%H%M%S")
+set -x
+
+CUR_DIR=$(cd $(dirname $0);pwd)
+
+export DATASET_PATH=/mnt/lustre/share_data/PAT/datasets/nlp/corpora/
+export VOCAB_PATH=./models/transformer/data/IWSLT/preprocessed/
+
+if [ -z $DATASET_PATH ]; then
+    echo "[ERROR] Please set DATASET_PATH"
+    echo "  IWSLT16: /mnt/lustre/share/datasets/nlp/corpora/"
+    exit 1
+fi
+
+if [ -z $VOCAB_PATH ]; then
+    echo "[ERROR] Please set VOCAB_PATH"
+    exit 1
+fi
+
+num_epochs=20
+iterations=-1
+print_freq=50
+dropout_rate=0.0
+batch_size=$(($1>=4?8:32))
+
+run_cmd="python -u ./models/transformer/train.py \
+    --log-path ./log/transformer \
+    --num_epochs $num_epochs \
+    --iterations $iterations \
+    --print-freq ${print_freq} \
+    --dropout_rate $dropout_rate \
+    --dataset-path $DATASET_PATH \
+    --vocab_path $VOCAB_PATH \
+    --ckp-path ./models/transformer/checkpoint \
+    --batch-size $batch_size \
+    "
+
+g=$(($1<8?$1:8))
+array=( $@ )
+len=${#array[@]}
+EXTRA_ARGS=${array[@]:1:$len}
+SRUN_ARGS=${SRUN_ARGS:-""}
+
+OMPI_MCA_mpi_warn_on_fork=0 GLOG_vmodule=MemcachedClient=-1
+mpirun -np $1 $run_cmd --launcher mpi ${EXTRA_ARGS}  2>&1 | tee log/transformer/train_transformer.log-$now:
